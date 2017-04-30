c3ab8d870c90d8944fae7bfae75e735f1f0fcf01
thebjorn <bp@datakortet.no>
2017-04-30 15:00:05 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name='pyimport',
+    version='0.1',
+    description='pyimport',
+    url='http://github.com/thebjorn/pyimport',
+    author='thebjorn',
+    license='MIT',
+    packages=['src'],
+    zip_safe=False
+)
---
+++ src/do_daily.py
@@ -0,0 +1,3 @@
+
+
+from src.db_backup import dev0_backup as dev0bk
